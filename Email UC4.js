850253e69c566ea9d774cdcc1271493ca89e8226
abhi5926 <abhisheksen5926@gmail.com>
2025-03-04 19:16:45 +0530

    [Abhishek] Add. Regex Program to Handle Optional Part in Email

---
+++ Email UC4.js
@@ -0,0 +1,8 @@
+function validateEmail(email){
+    const regex = /^[a-zA-Z0-9._+-]+@[a-zA-Z]+\.[a-zA-Z.]{2,}$/;
+    
+    return regex.test(email);
+}
+
+let email = "abs.xyz@bridgelabz.co.in";
+console.log(validateEmail(email));
